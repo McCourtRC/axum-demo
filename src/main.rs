62cf7f52022370e7bd797a29c32cac62a67eca01
Corey McCourt <mccourtrc@gmail.com>
2023-06-25 22:39:24 -0700

    fix: USE THE PORT

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,11 @@ async fn main() {
     // build our application with a single route
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
     let port = env::var("PORT").expect("missing PORT env var");
-    println!("listening on port {}", port);
+    let addr = format!("0.0.0.0:{}", port);
 
-    // run it with hyper on localhost:3000
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    println!("listening on addr {:?}", addr);
+
+    axum::Server::bind(&addr.parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
