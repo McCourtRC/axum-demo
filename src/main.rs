b3bac34abc5f3790f23fda3bf3d7ad4a6ef3348f
Corey McCourt <mccourtrc@gmail.com>
2023-06-25 22:58:19 -0700

    chore: cleanup setup

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
 use std::env;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
-use axum::{
-    routing::get,
-    Router,
-};
+use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-    let port = env::var("PORT").expect("missing PORT env var");
-    let addr = format!("0.0.0.0:{}", port);
+    let port = env::var("PORT").expect("Missing port number");
+    let port = port.parse::<u16>().expect("Invalid port given");
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), port);
 
-    println!("listening on addr {:?}", addr);
+    let app = Router::new().route("/", get(handler));
 
-    axum::Server::bind(&addr.parse().unwrap())
+    axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn handler() -> &'static str {
+    "Hello, World!"
 }
