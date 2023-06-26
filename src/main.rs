34fdbadd77646c6b383d7aafc783ee5890e7032c
Corey McCourt <mccourtrc@gmail.com>
2023-06-25 22:27:49 -0700

    config: use `PORT`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use axum::{
     routing::get,
     Router,
@@ -7,6 +9,8 @@ use axum::{
 async fn main() {
     // build our application with a single route
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let port = env::var("PORT").expect("missing PORT env var");
+    println!("listening on port {}", port);
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
